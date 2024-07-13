b57719f19862c3ab90a20ff4f1690a85c19a5a28
NeonCube <80168455+Neon-Cube@users.noreply.github.com>
2024-07-13 14:38:38 +0000

    Take better approach to override

--- Containerfile
+++ Containerfile
@@ -2,8 +2,6 @@ FROM ghcr.io/ublue-os/bazzite-deck:latest
 
 COPY build.sh /tmp/build.sh
 
-COPY packages/gamescope-3.14.2-1.fc40.x86_64.rpm /
-
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-rpm-ostree override replace --remove=gamescope gamescope-3.14.2-1.fc40.x86_64.rpm
+rpm-ostree override replace https://koji.fedoraproject.org/koji/buildinfo?buildID=2407683 # gamescope 3.14.2-1.fc40
