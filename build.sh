a9fde9866eb4c50ccad88e6cac18540fe7422ffb
NeonCube <80168455+Neon-Cube@users.noreply.github.com>
2024-07-14 02:42:28 +0330

    Fix gamescope-shaders conflict

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-rpm-ostree override remove gamescope
+rpm-ostree override remove gamescope gamescope-libs gamescope-shaders # HACK?
 rpm-ostree install https://koji.fedoraproject.org/koji/buildinfo?buildID=2407683 # gamescope 3.14.2-1.fc40
