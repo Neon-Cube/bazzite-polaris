6cdd9d0ef0548d3d6b74d74f686a977c2a02a90b
NeonCube <80168455+Neon-Cube@users.noreply.github.com>
2024-07-14 03:13:50 +0330

    Attempt dependency whack-a-mole

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-rpm-ostree override remove gamescope* # HACK?
+rpm-ostree override remove gamescope gamescope-libs gamescope-shaders gamescope-session-plus gamescope-session-steam # HACK?
 rpm-ostree install https://koji.fedoraproject.org/koji/buildinfo?buildID=2407683 # gamescope 3.14.2-1.fc40
