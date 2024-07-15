8b1d10b613ba83e0eef1e7a8a3c0e94b07af1259
NeonCube <80168455+Neon-Cube@users.noreply.github.com>
2024-07-15 21:07:26 +0330

    Add gamescope-session

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-rpm-ostree override remove gamescope gamescope-libs gamescope-shaders gamescope-session-plus gamescope-session-steam # HACK?
+rpm-ostree override remove gamescope gamescope-libs gamescope-shaders gamescope-session-plus gamescope-session-steam
 rpm-ostree install https://koji.fedoraproject.org/koji/buildinfo?buildID=2407683 # gamescope 3.14.2-1.fc40
+rpm-ostree install gamescope-session # Hacky solution to avoid forking
