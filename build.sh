7fc40b6b257c3ccf58f710767c70261a10fdabee
NeonCube <80168455+Neon-Cube@users.noreply.github.com>
2024-07-16 21:43:01 +0330

    Correct gamescope-session packages' installation
    Apply line seperation to build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 rpm-ostree override remove gamescope gamescope-libs gamescope-shaders gamescope-session-plus gamescope-session-steam
+
 rpm-ostree install https://koji.fedoraproject.org/koji/buildinfo?buildID=2407683 # gamescope 3.14.2-1.fc40
-rpm-ostree install gamescope-session # Hacky solution to avoid forking
+
+rpm-ostree install gamescope-session-plus gamescope-session-steam # Hacky solution to avoid forking
